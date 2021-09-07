099a48f07e6100f862f11d8f647fd1ff32debac5
RitikaT19 <ritika.tiwari@headstrait.com>
2021-09-07 18:39:54 +0530

    feat: styling for amount container

--- lib/main.dart
+++ lib/main.dart
@@ -30,8 +30,13 @@ class MyHomePage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
+              margin: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 15,
+              ),
               width: double.infinity,
               child: Card(
+                color: Colors.blue,
                 child: Text('CHART!'),
                 elevation: 5,
               ),
@@ -42,8 +47,24 @@ class MyHomePage extends StatelessWidget {
                   child: Row(
                     children: <Widget>[
                       Container(
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
                         child: Text(
                           tx.amount.toString(),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
                         ),
                       ),
                       Column(
