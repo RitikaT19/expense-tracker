8a6cecd384a1909aee32ff67c43aae5b94f82ab6
RitikaT19 <ritika.tiwari@headstrait.com>
2021-09-07 17:43:57 +0530

    feat: added card for transaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,14 +14,31 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(id: 't1', title: 'Books', amount: 400.00, date: DateTime.now()),
+    Transaction(id: 't2', title: 'Groceries', amount: 300.00, date: DateTime.now())
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Flutter App'),
         ),
-        body: Center(
-          child: Text('Widget Playground'),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                child: Text('CHART!'),
+                elevation: 5,
+              ),
+            ),
+            Column(children: transactions.map((tx){
+              return Card(child: Text( tx.title),);
+            }).toList(),)
+          ],
         ));
   }
 }
